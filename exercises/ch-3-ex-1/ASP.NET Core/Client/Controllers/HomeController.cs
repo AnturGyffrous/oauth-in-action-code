0e48797f8c358f9194df3c86f033751b1fcb25d1
Owain Williams <owain@hedfan.uk>
2019-08-28 18:15:14 +0100

    Add a HomeController with an Index action.

---
+++ exercises/ch-3-ex-1/ASP.NET Core/Client/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Client.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index() => View(new HomeViewModel { AccessToken = null, Scope = null });
+    }
+}
