62f715e25eec762d072bcfa53192e44547d08fb0
Owain Williams <owain@hedfan.uk>
2019-09-05 23:07:23 +0100

    Add 'words' endpoints to ProtectedResource controller.

--- exercises/ch-4-ex-2/ASP.NET Core/ProtectedResource/Controllers/ProtectedResourceController.cs
+++ exercises/ch-4-ex-2/ASP.NET Core/ProtectedResource/Controllers/ProtectedResourceController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ProtectedResource.Controllers
@@ -7,11 +11,36 @@ namespace ProtectedResource.Controllers
     [Authorize]
     public class ProtectedResourceController : Controller
     {
+        private static readonly Stack<string> SavedWords = new Stack<string>();
+
+        [HttpPost("words")]
+        public StatusCodeResult AddWord([FromForm] string word)
+        {
+            SavedWords.Push(word);
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        [HttpDelete("words")]
+        public StatusCodeResult DeleteWord()
+        {
+            SavedWords.Pop();
+            return NoContent();
+        }
+
+        [HttpGet("words")]
+        public object GetWords() =>
+            new
+            {
+                words = string.Join(" ", SavedWords),
+                timestamp = (int)(DateTime.UtcNow - DateTime.UnixEpoch).TotalSeconds
+            };
+
         [AllowAnonymous]
         [HttpGet("")]
         public IActionResult Index() => View();
 
         [HttpPost("resource")]
-        public object Resource() => new { name = "Protected Resource", description = "This data has been protected by OAuth 2.0" };
+        public object Resource() => new
+            { name = "Protected Resource", description = "This data has been protected by OAuth 2.0" };
     }
 }
