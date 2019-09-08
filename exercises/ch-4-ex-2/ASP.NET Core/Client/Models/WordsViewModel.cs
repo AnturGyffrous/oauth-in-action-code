af29a539dd864b068024d4adb8f207a509997e68
Owain Williams <owain@hedfan.uk>
2019-09-08 17:52:18 +0100

    Add a 'WordsViewModel' class and use it in the 'Words' view.

---
+++ exercises/ch-4-ex-2/ASP.NET Core/Client/Models/WordsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Client.Enums;
+
+namespace Client.Models
+{
+    public class WordsViewModel
+    {
+        public WordsResult? Result { get; set; }
+
+        public string Timestamp { get; set; }
+
+        public string Words { get; set; }
+    }
+}
