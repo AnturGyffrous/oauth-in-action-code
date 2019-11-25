d88b9e2ee2d991127ffe7281e4fdd1e1e251b583
Owain Williams <owain@hedfan.uk>
2019-11-25 23:11:50 +0000

    Add a 'ProduceViewModel' class and use it in the 'Produce' view.

---
+++ exercises/ch-4-ex-3/ASP.NET Core/Client/Models/ProduceViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Client.Models
+{
+    public class ProduceViewModel
+    {
+        public IEnumerable<string> Fruits { get; set; }
+
+        public IEnumerable<string> Meats { get; set; }
+
+        public string Scope { get; set; }
+
+        public IEnumerable<string> Veggies { get; set; }
+    }
+}
