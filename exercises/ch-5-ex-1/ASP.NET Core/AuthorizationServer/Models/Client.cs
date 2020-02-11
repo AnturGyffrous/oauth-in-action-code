2926660d116d9eb72e301d38a4ee70363b6306be
Owain Williams <owain@hedfan.uk>
2020-02-11 00:03:39 +0000

    Add a 'Client' model.

---
+++ exercises/ch-5-ex-1/ASP.NET Core/AuthorizationServer/Models/Client.cs
@@ -0,0 +1,13 @@
+﻿namespace AuthorizationServer.Models
+{
+    public class Client
+    {
+        public string ClientId { get; set; }
+
+        public string ClientSecret { get; set; }
+
+        public string RedirectUri { get; set; }
+
+        public string Scope { get; set; }
+    }
+}
