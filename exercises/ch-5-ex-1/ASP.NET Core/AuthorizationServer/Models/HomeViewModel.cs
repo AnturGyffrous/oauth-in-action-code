f8d6d23fe783c79b7a61c7be0f2c413e79d7ed8b
Owain Williams <owain@hedfan.uk>
2020-02-11 00:07:28 +0000

    Add a HomeViewModel and use in Index.cshtml.

---
+++ exercises/ch-5-ex-1/ASP.NET Core/AuthorizationServer/Models/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace AuthorizationServer.Models
+{
+    public class HomeViewModel
+    {
+        public AuthorizationServer AuthorizationServer { get; set; }
+
+        public IEnumerable<Client> Clients { get; set; }
+    }
+}
