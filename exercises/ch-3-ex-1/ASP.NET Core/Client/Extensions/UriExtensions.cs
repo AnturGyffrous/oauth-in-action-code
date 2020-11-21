d5f10e76ce46a220dcf7f9a20cca1c59330e7aa8
Owain Williams <owain@hedfan.uk>
2020-11-21 19:00:46 +0000

    Add a 'UriExtensions' class with an 'AddQueryString' extension method.

---
+++ exercises/ch-3-ex-1/ASP.NET Core/Client/Extensions/UriExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+
+namespace Client.Extensions
+{
+    public static class UriExtensions
+    {
+        public static Uri AddQueryString(this Uri uri, object queryString)
+        {
+            if (queryString == null)
+            {
+                return uri;
+            }
+
+            var parameters = HttpUtility.ParseQueryString(string.Empty);
+
+            queryString
+                .GetType()
+                .GetProperties()
+                .ToList()
+                .ForEach(x => parameters.Add(x.Name, x.GetValue(queryString, null).ToString()));
+
+            var uriBuilder = new UriBuilder(uri) { Query = parameters.ToString() };
+
+            return uriBuilder.Uri;
+        }
+    }
+}
---
+++ exercises/ch-3-ex-1/ASP.NET Core/Tests/ClientTests/Extensions/UriExtensionsTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Client.Extensions;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace ClientTests.Extensions
+{
+    public class UriExtensionsTests
+    {
+        [Fact]
+        public void AddQueryStringShouldDoNothingIfParameterIsNull()
+        {
+            // Arrange
+            var uri = new Uri("https://www.example.com");
+
+            // Act
+            var result = uri.AddQueryString(null);
+
+            // Assert
+            result.Should().Be(uri);
+        }
+
+        [Fact]
+        public void AddQueryStringShouldReflectOverParameterProperties()
+        {
+            // Arrange
+            var uri = new Uri("https://www.example.com");
+
+            // Act
+            var result = uri.AddQueryString(new
+                { userId = 3, email = "user+test@example.com", password = "Pas://&word=123" });
+
+            // Assert
+            result.Should().BeEquivalentTo(new Uri(
+                "https://www.example.com?userId=3&email=user%2btest%40example.com&password=Pas%3a%2f%2f%26word%3d123"));
+        }
+    }
+}
