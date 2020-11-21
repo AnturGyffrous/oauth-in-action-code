441eaf2000f2bd22131887c40eff8ffce7f4ecca
Owain Williams <owain@hedfan.uk>
2020-11-21 21:20:56 +0000

    Add a 'TokenResponse' class.

---
+++ exercises/ch-3-ex-1/ASP.NET Core/Client/Authentication/OAuth/TokenResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace Client.Authentication.OAuth
+{
+    [DataContract]
+    public class TokenResponse
+    {
+        [DataMember(Name = "access_token")]
+        public string AccessToken { get; set; }
+
+        [DataMember(Name = "token_type")]
+        public string TokenType { get; set; }
+    }
+}
