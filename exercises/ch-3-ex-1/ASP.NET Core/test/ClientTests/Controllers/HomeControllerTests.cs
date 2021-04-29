086755e41f5948a4ac80459e5996fe8821086dee
Owain Williams <owain@hedfan.uk>
2021-04-29 23:44:03 +0100

    Use the WebApplicationFactory so a TestServer can be created.

--- exercises/ch-3-ex-1/ASP.NET Core/test/ClientTests/Controllers/HomeControllerTests.cs
+++ exercises/ch-3-ex-1/ASP.NET Core/test/ClientTests/Controllers/HomeControllerTests.cs
@@ -1,9 +1,20 @@
+using Client;
+
+using Microsoft.AspNetCore.Mvc.Testing;
+
 using Xunit;
 
 namespace ClientTests.Controllers
 {
-    public class HomeControllerTests
+    public class HomeControllerTests : IClassFixture<WebApplicationFactory<Startup>>
     {
+        public HomeControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        private readonly WebApplicationFactory<Startup> _factory;
+
         [Fact]
         public void Test1()
         {
